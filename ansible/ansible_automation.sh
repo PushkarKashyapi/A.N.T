2f135aa26ea492006356f9f40fddf568a1c6a122
Pushkar Kashyapi <141157548+PushkarKashyapi@users.noreply.github.com>
2025-06-14 02:13:21 +0530

    Add files via upload

--- ansible/ansible_automation.sh
+++ ansible/ansible_automation.sh
@@ -1,1 +1,18 @@
+!/bin/bash
+
+read -p 'please enter target user:' USER
+
+read -p 'please enter target ip address:' IP
+
+
+sudo dnf install epel-release -y
+
+sudo dnf install ansible -y
+
+sudo dnf install sshd -y
+
+
+ssh-keygen -t rsa -b 2048
+
+ssh-copy-id "$USER@$IP"
 
